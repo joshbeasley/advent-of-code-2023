0cd4b8cec6c3d177105c7485a009bc0209b49498
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-18 21:25:09 -0800

    4 - midway

--- 4/4.py
+++ 4/4.py
@@ -1,3 +1,5 @@
+from math import log2
+
 file = open("cards.txt", "r")
 lines = file.readlines()
 
@@ -8,14 +10,35 @@ for line in lines:
   mine.append(m.split())
 
 total = 0
+record = {}
+num_win = []
 for i in range(len(winners)):
   winnings = 0
+  num_winners = 0
   for j in range(len(mine[i])):
     if mine[i][j] in winners[i]:
-      if winnings == 0:
-        winnings += 1
-      else:
-        winnings *= 2
-  total += winnings
+      winnings = 2**num_winners
+      num_winners += 1 
+  record[i] = winnings
+  num_win.append(num_winners)
+print(sum(record.values()))
+print(num_win)
+
+total = len(winners)
+print(total)
+
+def process_scratchcard(i):
+  if num_win[i] == 0:
+    return 0
+  total = num_win[i]
+  for j in range(i+1, total+1):
+    total += process_scratchcard(j)
+  return total
+
+for i in range(len(num_win)):
+  total += process_scratchcard(i)
 
 print(total)
+
+
+# i = 4, len(record) = 5, num_winner = 3
