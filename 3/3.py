2a9a48fca47e8f2573bb75cc09d1400b26557e95
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-20 15:10:04 -0800

    3 - part two

--- 3/3.py
+++ 3/3.py
@@ -3,24 +3,41 @@ import re
 grid = open('engine.txt').read().splitlines()
 cs = set()
 
+ratios = []
 for r, row in enumerate(grid):
   for c, ch in enumerate(row):
-    if ch.isdigit() or ch == '.':
-      continue
-    for cr in [r - 1, r, r + 1]:
-      for cc in [c - 1, c, c + 1]:
-        if cr < 0 or cr >= len(grid) or cc < 0 or cc >= len(grid[cr]) or not grid[cr][cc].isdigit():
-          continue
-        while cc > 0 and grid[cr][cc - 1].isdigit():
-          cc -= 1
-        cs.add((cr, cc))
+    if ch == "*":
+      ratio = set()
+      for cr in [r - 1, r, r + 1]:
+        for cc in [c - 1, c, c + 1]:
+          if cr < 0 or cr >= len(grid) or cc < 0 or cc >= len(grid[cr]) or not grid[cr][cc].isdigit():
+            continue
+          while cc > 0 and grid[cr][cc - 1].isdigit():
+            cc -= 1
+          cs.add((cr, cc))
+          ratio.add((cr, cc))
+      ratios.append(ratio)
 
-ns = []
-for r, c in cs:
-  num = ''
-  while c < len(grid[r]) and grid[r][c].isdigit():
-    num += grid[r][c]
-    c += 1
-  ns.append(int(num))
+total = 0
+for ratio in ratios:
+  if len(ratio) == 2:
+    product = 1
+    for r, c in ratio:
+      num = ''
+      while c < len(grid[r]) and grid[r][c].isdigit():
+        num += grid[r][c]
+        c += 1
+      product *= int(num)
+    total += product
 
-print(sum(ns))
+print(total)
+
+# ns = []
+# for r, c in cs:
+#   num = ''
+#   while c < len(grid[r]) and grid[r][c].isdigit():
+#     num += grid[r][c]
+#     c += 1
+#   ns.append(int(num))
+
+# print(sum(ns))
