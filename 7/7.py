3f604e0fe483c9a337cddef226883dac4ec63e8f
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-20 22:51:29 -0800

    7 - part one

--- 7/7.py
+++ 7/7.py
@@ -1,3 +1,44 @@
-file = open('hands.txt').read().split('\n')
+file = open('hands.txt')
 
-hands, 
+vals = {'T':'A', 'J':'B', 'Q':'C', 'K':'D', 'A':'E'}
+
+scores = []
+plays = []
+for line in file.readlines():
+  hand, bid = line.split()
+  plays.append((hand, int(bid)))
+  counts = {}
+  score = -1
+  for card in hand:
+    if card in counts:
+      counts[card] += 1
+    else:
+      counts[card] = 1
+  if len(counts) == 1:
+    score = 7
+  elif len(counts) == 2:
+    if 2 in counts.values():
+      score = 5
+    else:
+      score = 6
+  elif len(counts) == 3:
+    if 3 in counts.values():
+      score = 4
+    else:
+      score = 3
+  elif len(counts) == 4:
+    score = 2
+  else:
+    score = 1
+    
+  scores.append((score, [vals.get(char, char) for char in hand]))
+  
+print(scores)
+  
+plays  = [x for _, x in sorted(zip(scores, plays))]
+
+total = 0
+for rank, (hand, bid) in enumerate(plays):
+  total += (rank + 1) * bid
+
+print(total)
