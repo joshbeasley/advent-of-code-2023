46b77570a5303fc8e68f9a9ad4ca3309b7de41ed
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-20 21:53:02 -0800

    6 - part one

--- 6/6.py
+++ 6/6.py
@@ -1,4 +1,15 @@
-time, distance = open('races.txt').read().split('\n')
+# Part 1
+times, distances = open('races.txt').read().split('\n')
 
-time = list(map(int, time.split(':')[1].split()))
-time = list(map(int, time.split(':')[1].split()))
+times = list(map(int, times.split(':')[1].split()))
+distances = list(map(int, distances.split(':')[1].split()))
+
+total = 1
+for idx, time in enumerate(times):
+  records = 0
+  for speed in range(1, time-1):
+    if speed * (time - speed) > distances[idx]:
+      records += 1
+  total *= records
+  
+print(total)
