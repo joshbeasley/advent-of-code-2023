f073e59d775ad311bd19e40a453cbb437ade0aec
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-20 21:56:29 -0800

    6 - part two

--- 6/6.py
+++ 6/6.py
@@ -13,3 +13,15 @@ for idx, time in enumerate(times):
   total *= records
   
 print(total)
+
+# Part 2
+times, distances = open('races.txt').read().split('\n')
+
+time = int(''.join(times.split(':')[1].split()))
+distance = int(''.join(distances.split(':')[1].split()))
+
+records = 0
+for speed in range(1, time - 1):
+  if speed * (time - speed) > distance:
+    records += 1
+print(records)
