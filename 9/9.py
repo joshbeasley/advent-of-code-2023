316e9ed26fee2182d881ba586778857ac455b848
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-22 12:46:47 -0800

    9 - part two

--- 9/9.py
+++ 9/9.py
@@ -1,6 +1,8 @@
 file = open('oasis.txt')
 lines = file.readlines()
 
+# Part 1
+
 def generate_differences(values):
     diffs = []
     diffs.append(values)
@@ -18,12 +20,28 @@ def extrapolate(diffs):
         diffs[i].append(diffs[i][-1] + diffs[i-1][-1])
     return diffs
 
+# total = 0
+# for line in lines:
+#     values = list(map(int, line.split()))
+#     diffs = generate_differences(values)
+#     extrapolation = extrapolate(diffs)
+#     total += extrapolation[-1][-1]
+    
+# print(total)
+
+# Part 2
+
+def extrapolate_backwards(diffs):
+    diffs.reverse()
+    for i in range(1, len(diffs)):
+        diffs[i].insert(0, diffs[i][0] - diffs[i-1][0])
+    return diffs
+
 total = 0
 for line in lines:
     values = list(map(int, line.split()))
     diffs = generate_differences(values)
-    extrapolation = extrapolate(diffs)
-    total += extrapolation[-1][-1]
+    extrapolation = extrapolate_backwards(diffs)
+    total += extrapolation[-1][0]
     
 print(total)
-    
