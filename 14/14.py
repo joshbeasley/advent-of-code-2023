f67c8f3800f2e2f7349510fa00880619d5bfc79a
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-23 15:24:06 -0800

    14 - part two

--- 14/14.py
+++ 14/14.py
@@ -20,10 +20,44 @@ def roll_rock(r, c, grid):
     grid[r-count][c] = 'O'
     return grid
 
-for i in range(1, len(grid)):
-    for j in range(len(grid[0])):
-        if grid[i][j] == 'O':
-            grid = roll_rock(i, j, grid)
+def print_grid(grid):
+    for row in grid:
+        print(''.join(row))
+
+def to_tuple(grid):
+    grid = [tuple(row) for row in grid]
+    return tuple(grid)
+
+def to_list(grid):
+    grid = [list(row) for row in grid]
+    return grid
+
+def single_cycle(grid):
+    for _ in range(4):
+        for i in range(1, len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 'O':
+                    grid = roll_rock(i, j, grid)
+        grid = list(zip(*grid))
+        grid = [list(row[::-1]) for row in grid]
+    return grid
+
+seen = {to_tuple(grid)}
+grids = [to_tuple(grid)]
+count = 0
+while True:
+    count += 1
+    grid = single_cycle(grid)
+    grid = to_tuple(grid)
+    if grid in seen:
+        break
+    seen.add(grid)
+    grids.append(grid)
+    grid = to_list(grid)
+
+first = grids.index(grid)
+
+grid = grids[(1000000000 - first) % (count - first) + first]
 
 total = 0
 for i in range(len(grid)):
