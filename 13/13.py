7165b21f3307fa47f1e1a7a870e49327b3d30099
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-23 14:00:02 -0800

    13 - part two

--- 13/13.py
+++ 13/13.py
@@ -6,8 +6,16 @@ def find_mirror(grid):
         above = above[:len(below)]
         below = below[:len(above)]
         
-        if above == below:
+        mismatches = 0
+        for r in range(len(above)):
+            for c in range(len(above[0])):
+                if above[r][c] != below[r][c]:
+                    mismatches += 1
+        
+        if mismatches == 1:
             return i
+        # if above == below:
+        #     return i
     return 0
 
 total = 0
