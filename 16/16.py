127d216ced336a2750679eff8a5050233d8c86d5
Josh Beasley <joshua.beasley@km-sscdev.org>
2023-12-24 05:21:50 -0800

    16 - part one

---
+++ 16/16.py
@@ -0,0 +1,97 @@
+grid = open('grid.txt').read().splitlines()
+
+seen = set()
+q = []
+
+# iterative approach
+initial = (0, -1, 0, 1)
+q.append(initial)
+seen.add(initial)
+
+while len(q) > 0:
+    r, c, dr, dc = q.pop(0)
+    
+    r += dr
+    c += dc
+    
+    if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+        continue
+    
+    val = grid[r][c]
+    
+    if val == '.' or (val == '-' and dc != 0) or (val == '|' and dr != 0):
+        if (r,c,dr,dc) not in seen:
+            q.append((r,c,dr,dc))
+            seen.add((r,c,dr,dc))
+    elif val == '\\':
+        dr, dc = dc, dr
+        if (r,c,dr,dc) not in seen:
+            q.append((r,c,dr,dc))
+            seen.add((r,c,dr,dc))
+    elif val == '/':
+        dr, dc = -dc, -dr
+        if (r,c,dr,dc) not in seen:
+            q.append((r,c,dr,dc))
+            seen.add((r,c,dr,dc))
+    else:
+        if val == '|':
+            for dr, dc in [(1, 0), (-1, 0)]:
+                if (r,c,dr,dc) not in seen:
+                    q.append((r,c,dr,dc))
+                    seen.add((r,c,dr,dc))   
+        else:
+            for dr, dc in [(0, 1), (0, -1)]:
+                if (r,c,dr,dc) not in seen:
+                    q.append((r,c,dr,dc))
+                    seen.add((r,c,dr,dc)) 
+
+print(len(set([(i[0], i[1]) for i in seen])) - 1)
+    
+
+# recursive approach
+def energized(r, c, direction):
+    if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+        return 0
+    if (r,c, direction) in seen:
+        return 0
+    seen.add((r,c, direction))
+    if grid[r][c] == '.' or ((direction == 'left' or direction == 'right') and grid[r][c] == '-') or ((direction == 'up' or direction =='down') and grid[r][c] == '|'):
+        if direction == 'up':
+            return 1 + energized(r-1, c, 'up')
+        if direction == 'down':
+            return 1 + energized(r+1, c, 'down')
+        if direction =='left':
+            return 1 + energized(r, c-1, 'left')
+        if direction == 'right':
+            return 1 + energized(r, c+1, 'right')
+    if grid[r][c] == "\\":
+        if direction == 'up':
+            return 1 + energized(r, c-1, 'left')
+        if direction == 'down':
+            return 1 + energized(r, c+1, 'right')
+        if direction =='left':
+            return 1 + energized(r-1, c, 'up')
+        if direction == 'right':
+            return 1 + energized(r+1, c, 'down')
+    if grid[r][c] == '/':
+        if direction == 'up':
+            return 1 + energized(r, c+1, 'right')
+        if direction == 'down':
+            return 1 + energized(r, c-1, 'left')
+        if direction =='left':
+            return 1 + energized(r+1, c, 'down')
+        if direction == 'right':
+            return 1 + energized(r-1, c, 'up')
+    if grid[r][c] == '|' and (direction == 'left' or direction == 'right'):
+        return 1 + energized(r-1, c, 'up') + energized(r+1, c, 'down')
+    if grid[r][c] == '-' and (direction == 'up' or direction == 'down'):
+        return 1 + energized(r, c-1, 'left') + energized(r, c+1, 'right')
+
+# energized(0,0,'right')
+# print(seen)
+# print(len(seen))
+# print(len(set([(i[0], i[1]) for i in seen])))
+
+    
+    
+            
